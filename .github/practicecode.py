339f2cbe2b426480105fad5ed52d289a91236c3a
EmaanAhmad27 <emaanahmad327@gmail.com>
2024-11-22 15:37:58 +0000

    fgf

--- .github/practicecode.py
+++ .github/practicecode.py
@@ -2,5 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 url = "https://saeedghani.pk/"
 r = requests.get(url)
-soup = BeautifulSoup(r.text, 'lxml')
-print (soup)
+soup = BeautifulSoup(r.text, "lxml")
+# print (soup.div)
+
+#tag = soup.header
+#print (tag.attrs)
+
