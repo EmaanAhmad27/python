ddd06ebe83906359b42f970113b20bb12702e429
EmaanAhmad27 <emaanahmad327@gmail.com>
2024-11-26 10:16:05 +0000

    4th project

--- .github/practiceproject.py
+++ .github/practiceproject.py
@@ -1,9 +1,41 @@
-Joke = "Here is a joke for you! Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs"
-user_input = input ("What do you want?:")
-def joke_recognition():
-    if "joke" in user_input:
-        print (Joke)
-    else:
-        print ("Sorry! I only tell jokes")
-joke_recognition()
+
+# def joke_recognition():
+#     Joke = "Here is a joke for you! Sophia is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Sophia returns with 13 liters of milk. The programmer asks why and Sophia replies: 'because they had eggs"
+#     user_input = input ("What do you want?:")
+#     if "joke" in user_input:
+#         print (Joke)
+#     else:
+#         print ("Sorry! I only tell jokes")
+
+
+# def double_num():
+#     while True:
+#         user_num = int(input ("Enter a number to double it:"))
+#         if user_num < 100:
+#             user_num = user_num * 2
+#             print (user_num)
+#         else:
+#             print ("enter a number less than 100")
+#             break
+
+# def space_ship ():       
+#     for i in range (10, 0, -1):
+#         print (i)
+#     print ("LIFT OFF")
+# space_ship()
+
+# import random
+# def guess_num ():
+#     while True:
+#         guess: int = random.randint (1, 99)
+#         user_input = int(input("Guess the number:"))
+#         if user_input < guess:
+#             print ("Your guess is too low")
+#         elif user_input > guess:
+#             print ("Your guess is too high")
+#         else:
+#             print (f"Congrats! The number was {guess}")
+#             break
+# guess_num()
+
 
