36102501c2361bc7fd5c4653a70f30a5b4883f6d
EmaanAhmad27 <emaanahmad327@gmail.com>
2025-01-31 18:44:44 +0000

    fast

--- FastAPI.py
+++ FastAPI.py
@@ -1,34 +1,52 @@
+# from fastapi import FastAPI
+# from langchain_google_genai import GoogleGenerativeAI
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+# app = FastAPI()
+# llm = GoogleGenerativeAI(model = "gemini-1.5-flash", api_key = os.getenv("GoogleAPIKey"))
+# @app.get("/chat")
+# def chat (query:str):
+#    prompt = "you are a tutor of programming userquery:"
+#    result= llm.invoke(prompt + query)
+#    return {result}
+#/docs is needed after the port name to get the answer.
 from fastapi import FastAPI
-from typing import Union, Optional
-from pydantic import BaseModel
-
-class User(BaseModel): #this will be our dictionary which has variable and data type of our choice
-   name: Optional[str] #this parameter is optional but if it is passed my user it is string.
-   age: int
-app = FastAPI() #to store it in a variable
+app = FastAPI()
 @app.get("/")
-def chat():
-    return {
-       "name": "Emaan"
-    }
-@app.get("/auth/login")
-def login():
-   print("Login function calling")
-   return ("Login function calling")
+def chat ():
+   return {"emaan"}
 
-@app.get("/auth/register")
-def register (username:User): #to add value directly
-   return {"username":username}
+# @app.get("/auth/{id}")
+# def login (id):
+#    print(id)
+#    return {"function is calling"}
 
 # @app.get("/auth/register")
-# def register (value:int,username:User,count:int | None = None): #to add value directly
-#    print (value)
-#    return {"value":value, "username":username, "count":count}
+# def register (val, username, count):
+#    print(val, username, count)
+#    return val, username, count
 
-@app.get("/auth/{id}")
-def login(id): 
-   print(id)
-   return ("Login function calling")
+
+from typing import Union
+
+@app.get("/auth/register")
+def register (val:int, username:str, count:int | None = None):
+   print(val, username, count)
+   return val, username, count
+# from pydantic import BaseModel
+
+# class User(BaseModel): #this will be our dictionary which has variable and data type of our choice
+#    name: Optional[str] #this parameter is optional but if it is passed my user it is string.
+#    age: int
+
+
+
+
+
+
+
+
 
 
 
